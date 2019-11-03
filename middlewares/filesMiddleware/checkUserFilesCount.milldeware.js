3f9024a6cde6b2a1c99d2affbe1ddd3ced53357d
MachkivskyiYaroslav <machkyvskiy@outlook.com>
2019-11-03 19:12:41 +0200

    update req

--- middlewares/filesMiddleware/checkUserFilesCount.milldeware.js
+++ middlewares/filesMiddleware/checkUserFilesCount.milldeware.js
@@ -1,7 +1,7 @@
 const ErrorHandler =require('../../error/ErrorHandler');
 
 module.exports = (req, res, next) => {
-    const photos = req.files.photos;
+    const photos = req.files.photo;
 
     if (photos.length > 1) {
         return next(new ErrorHandler(`You can upload just one photo`, 400, 'checkUserFilesCount'))
